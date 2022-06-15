2cdab623f5287a174f991188f9ea47ab2023ffbc
Ankur Raiyani <ankur@websoptimization.com>
2022-06-15 12:36:06 +0530

    Initial Commit

---
+++ ngo-admin-system/src/main/java/com/websopti/ngosys/NgoAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.websopti.ngosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoAdminSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoAdminSystemApplication.class, args);
+	}
+
+}
---
+++ ngo-admin-system/src/test/java/com/websopti/ngosys/NgoAdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websopti.ngosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoAdminSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
