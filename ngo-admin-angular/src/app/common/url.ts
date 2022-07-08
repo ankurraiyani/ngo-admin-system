ee71f9f1e47a093251a1fa00fb6fcf31050facec
Masum <masumpatel1911@gmail.com>
2022-07-08 14:14:29 +0530

    Solved conflict Volunteer Validation

--- ngo-admin-angular/src/app/common/url.ts
+++ ngo-admin-angular/src/app/common/url.ts
@@ -8,15 +8,11 @@ export const VOLUNTEER_GETID : string = BASE_URL + "volunteer/get";
 export const VOLUNTEER_ISACTIVE_DEACTIVE: string = BASE_URL + "volunteer/volunteerActiveDeactive";
 export const GET_ALL_ACTIVE_VOLUNTEER:string = BASE_URL + "volunteer/getAll/active"
 
-
-
 //event apis
 export const EVENT_ADD: string = BASE_URL + "event/save";
 export const EVENT_GETALL:string= BASE_URL + "event/get";
-
 export const EVENT_DELETE: string=BASE_URL+"event/delete";
 export const EVENT_GETID: string=BASE_URL+"event/get"
-
 
 //employee apis
 export const EMPLOYEE_ADD: string = BASE_URL + "employee/save";
@@ -25,11 +21,9 @@ export const EMPLOYEE_DELETE:string= BASE_URL + "employee/deleteId";
 export const EMPLOYEE_ISACTIVE_DEACTIVE: string = BASE_URL + "employee/employeeActiveDeactive";
 export const GET_ALL_ACTIVE_EMPLOYEE: string = BASE_URL + "employee/getAll/active";
 
-
 //donner apis
 export const DONER_ADD:string =BASE_URL+"doner/save";
 export const DONER_GETALL:string =BASE_URL+"doner/get";
 export const DONER_DELETE: string=BASE_URL+"doner/delete";
 export const DONER_GETID:string=BASE_URL+"doner/get";
 export const GET_ALL_ACTIVE_DONER:string=BASE_URL+"doner/getAll/active";
-
